d7df691ae8d843f26c520bf7cc372ddfe9ac4f76
Stanislav_Lushnikov <lust48h@gmail.com>
2023-10-12 22:16:51 +0300

    Fix exeption: InputDataAmountException, PersonSexException, PersonSurnameException,

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,8 +9,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        try{
-            Scanner scanner = new Scanner(System.in);
+        try(Scanner scanner = new Scanner(System.in)){
+
             System.out.println("Введите следующие данные через пробел ");
             System.out.println("Фамилия Имя Отчество датарождения номертелефона пол: ");
             String input = scanner.nextLine();
@@ -18,100 +18,93 @@ public class Main {
 
             Person person1 = new Person(data);
             try{
-                person1.writeInFile("myTextFile.txt");
+                person1.writeInFile(person1.surname + ".txt");
             }
             catch (WriteFileException e){
-                System.out.printf("Исключение при попытки записи в файл %s", e.getFileName());
+                System.out.printf("Исключение при попытки записи в файл %s\n", e.getFileName());
+                System.out.println(e.getStackTrace());
             }
-            scanner.close();
         }
         catch (InputDataAmountException e){
-            System.out.printf("Исключение при вводе количества данных человека, %s из 6", e.getCurrentAmount());
+            System.out.printf("Исключение при вводе количества данных человека, %s вместо 6\n", e.getCurrentAmount());
+            System.out.println(e.getMessage());
         }
         catch (PersonSurnameException e){
-            System.out.printf("Исключение при вводе фамилии человека %s", e.surname);
+            System.out.printf("Исключение при вводе фамилии %s\n", e.getSurname());
+            System.out.println(e.getMessage());
         }
         catch (PersonNameException e){
-            System.out.printf("Исключение при вводе имени человека %s", e.getName());
+            System.out.printf("Исключение при вводе имени %s\n", e.getName());
+            System.out.println(e.getMessage());
         }
         catch (PersonPatronymicException e){
-            System.out.printf("Исключение при вводе отчества человека %s", e.getPatronymic());
+            System.out.printf("Исключение при вводе отчества %s\n", e.getPatronymic());
+            System.out.println(e.getMessage());
         }
         catch (PersonBirtdayException e) {
-            System.out.printf("Исключение при вводе даты дня рождения человека %s", e.getBirthday());
+            System.out.printf("Исключение при вводе даты дня рождения %s\n", e.getBirthday());
+            System.out.println(e.getMessage());
         }
         catch (PersonPhoneException e) {
-            System.out.printf("Исключение при вводе номера телефона человека %s", e.getPhone());
+            System.out.printf("Исключение при вводе номера телефона %s\n", e.getPhone());
+            System.out.println(e.getMessage());
         }
         catch (PersonSexException e) {
-            System.out.printf("Исключение при вводе пола человека %s", e.getSex());
+            System.out.printf("Исключение при вводе пола человека %s\n", e.getSex());
+            System.out.println(e.getMessage());
         }
     }
 
 }
 
-/*
-class String[] DataReader(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите следующие данные через пробел ");
-        System.out.println("Фамилия Имя Отчество датарождения номертелефона пол: ");
-        String input = scanner.nextLine();
-
-        String[] words = input.split(" ");
-        return words;
-        }
-
- */
 abstract class PersonData {
     String[] data;
     String surname;
     String name;
     String patronymic;
     String birthday;
-    int phone;
-    char sex;
+    String phone;
+    String sex;
 
     final int DATAELEMENTS = 6;
     final String datePattern = "dd/MM/yyyy";
 
     public PersonData(String[] data) throws InputDataAmountException, PersonSurnameException, PersonNameException,
             PersonPatronymicException, PersonBirtdayException, PersonPhoneException, PersonSexException {
-        if (!(data.length == DATAELEMENTS)) {
+        if (!(data.length == DATAELEMENTS))
             throw new InputDataAmountException("Передано некорректное количество данных", data.length);
-        }
         this.data = data;
         for (int i = 0; i < data[0].length(); i++) {
-            if (!Character.isLetter(data[0].toCharArray()[i])) {
+            if (!Character.isLetter(data[0].charAt(i)))
                 throw new PersonSurnameException("В фамилии человека указаны некорректные символы", data[0]);
-            }
         }
         this.surname = data[0];
+
         for (int i = 0; i < data[1].length(); i++) {
-            if (!Character.isLetter(data[1].toCharArray()[i])) {
+            if (!Character.isLetter(data[1].charAt(i)))
                 throw new PersonNameException("В имени человека указаны некорректные символы", data[1]);
-            }
         }
         this.name = data[1];
+
         for (int i = 0; i < data[2].length(); i++) {
-            if (!Character.isLetter(data[2].toCharArray()[i])) {
+            if (!Character.isLetter(data[2].charAt(i)))
                 throw new PersonPatronymicException("В отчестве человека указаны некорректные символы", data[2]);
-            }
         }
         this.patronymic = data[2];
-        if (!data[3].matches("\\d{2}/\\d{2}/\\d{4}")) {
+
+        if (!data[3].matches("\\d{2}/\\d{2}/\\d{4}"))
             throw new PersonBirtdayException("Переданы некорректные данные даты рождения", data[3]);
-        }
         this.birthday = data[3];
+
         for (int i = 0; i < data[4].length(); i++) {
-            if (!Character.isDigit(data[4].toCharArray()[i])) {
+            if (!Character.isDigit(data[4].charAt(i)))
                 throw new PersonPhoneException("В номере указаны некорректные символы", data[4]);
-            }
         }
-        this.phone = Integer.getInteger(data[4]);
-        if ((data[5] == "m") || (data[5] == "f")){
-            this.sex = data[5].charAt(0);
-        }
-        throw new PersonSexException("Пол человека указан не корректно", data[5]);
+        this.phone = data[4];
+
+        if (!((data[5].equals("m")) | (data[5].equals("f"))))
+            throw new PersonSexException("Пол человека указан не корректно", data[5]);
+        this.sex = data[5];
     }
 
     abstract void writeInFile(String fileName) throws WriteFileException;
@@ -125,16 +118,16 @@ class Person extends PersonData {
         this.name = data[1];
         this.patronymic = data[2];
         this.birthday = data[3];
-        this.phone = Integer.parseInt(data[4]);
-        this.sex = data[5].charAt(0);
+        this.phone = data[4];
+        this.sex = data[5];
     }
 
     @Override
     void writeInFile(String fileName) throws WriteFileException {
         try(FileWriter writer = new FileWriter(fileName, true))
         {
-            writer.write(data[0] + " " + data[1] + " " + data[2] + " " + data[3] + " " + data[4] + " " + data[5]
-                    + " " + data[6] + "\n");
+            writer.write(data[0] + " " + data[1] + " " + data[2] + " " + data[3] + " " + data[4]
+                    + " " + data[5] + "\n");
             writer.flush();
         }
         catch(IOException ex){
